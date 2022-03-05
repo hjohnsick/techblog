2033f2007e146fc84766b3b9ad64137fc4387d32
hjohnsick <hjohnsick@yahoo.com>
2022-03-05 11:58:32 -0500

    added validation to login

--- models/User.js
+++ models/User.js
@@ -3,7 +3,11 @@ const sequelize = require("../config/connection");
 const bcrypt = require("bcrypt");
 
 // create our User model
-class User extends Model {}
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 // define table columns and configuration
 User.init(
@@ -45,6 +49,13 @@ User.init(
       async beforeCreate(newUserData) {
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
+      },
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
+        return updatedUserData;
       },
     },
     // table configuration options go here
