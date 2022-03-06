f0803ca4e5067d6a336ce6328f2f8c8c96cf3184
hjohnsick <hjohnsick@yahoo.com>
2022-03-06 14:25:08 -0500

    fixed folder structure

---
+++ public/javascript/login.js
@@ -0,0 +1,32 @@
+console.log("File is loaded");
+
+async function signupFormHandler(event) {
+  event.preventDefault();
+
+  const username = document.querySelector("#username-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+  console.log(username, email, password);
+  if (username && email && password) {
+    const response = await fetch("/api/users", {
+      method: "post",
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    // check the response status
+    if (response.ok) {
+      console.log(response);
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
