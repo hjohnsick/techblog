67a77d5aa8261b09ffc6491776fe9e259ff134b9
hjohnsick <hjohnsick@yahoo.com>
2022-03-06 15:17:55 -0500

    added dashboard template

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -32,6 +32,36 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/dashboard", (req, res) => {
+  console.log(req.session);
+  Post.findAll({
+    attributes: ["id", "title", "contents", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      // pass a single post object into the homepage template
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("dashboard", { posts });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
     res.redirect("/");
