58b30a5431e647c4c8957478560b4f0dbd236e9c
hjohnsick <hjohnsick@yahoo.com>
2022-03-06 14:55:44 -0500

    added more functionality to login

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,6 +3,7 @@ const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
 
 router.get("/", (req, res) => {
+  console.log(req.session);
   Post.findAll({
     attributes: ["id", "title", "contents", "created_at"],
     include: [
@@ -32,6 +33,11 @@ router.get("/", (req, res) => {
 });
 
 router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
   res.render("login");
 });
 
